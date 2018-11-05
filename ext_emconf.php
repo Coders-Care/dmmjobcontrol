84b0ba1cf9032c5b2d4556f0ab3ab55afa5ef07c
Cybercraft <info@cybercraft.de>
2018-11-05 20:14:31 +0100

    [TASK] Change required TYPO3 version to be compatible with CMS 6.2.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-6.2.0',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
